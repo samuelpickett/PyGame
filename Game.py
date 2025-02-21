d13f1f842bb169eb20baa78c22fd423db55a822e
samuelpickett <pic21007@byui.edu>
2025-02-20 20:41:42 -0700

    Finished the game

--- Game.py
+++ Game.py
@@ -9,12 +9,13 @@ white_color = (255,255,255)
 black_color = (0,0,0)
 # Sets the clock rate for game
 clock = pygame.time.Clock()
-
+pygame.font.init()
+font = pygame.font.SysFont("comicsans", 75)
 
 class Game():
     tick_rate = 60
     direction = 0
-    def __init__(self, title, width, height):
+    def __init__(self, image_path, title, width, height):
         self.title = title
         self.width = width
         self.height = height
@@ -24,12 +25,22 @@ class Game():
         # Set the game window color to white
         self.game_screen.fill(white_color)
         pygame.display.set_caption(title)
-    def run_game_loop(self):
+        
+        background_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(background_image, (width, height))
+    
+    def run_game_loop(self, level_speed):
         player = PlayerCharacter("images/player.png", 375, 700, 50, 50)
-        enemy0 = EnemyCharacter("images/enemy.png", 20, 400, 50, 50)
+        enemy0 = EnemyCharacter("images/enemy.png", 20, 600, 50, 50)
+        enemy0.speed *= level_speed
+        enemy1 = EnemyCharacter("images/enemy.png", self.width - 50, 400, 50, 50)
+        enemy1.speed *= level_speed
+        enemy2 = EnemyCharacter("images/enemy.png", 20, 200, 50, 50)
+        enemy2.speed *= level_speed
         treasure = GameObject("images/treasure.png", 375, 50, 50,50)
         direction = 0
         is_game_over = False
+        did_win = False
         # Main game loop
         while not is_game_over:
             # checks for events
@@ -48,22 +59,44 @@ class Game():
                     if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                         direction = 0
             self.game_screen.fill(white_color)
+            self.game_screen.blit(self.image,(0,0))
             treasure.draw(self.game_screen)
             # Updates the player's position
             player.move(direction, self.height)
             player.draw(self.game_screen)
             enemy0.move(self.width)
             enemy0.draw(self.game_screen)
+            if level_speed >= 2:
+                enemy1.move(self.width)
+                enemy1.draw(self.game_screen)
+            if level_speed >= 4:
+                enemy1.move(self.width)
+                enemy1.draw(self.game_screen)
             
             # Detects if the player is colliding with an object and ends the game
             if player.detect_collision(enemy0):
                 is_game_over = True
+                text = font.render("You lose", True, black_color)
+                self.game_screen.blit(text, (275,350))
+                pygame.display.update()
+                clock.tick(1)
+                break
             elif player.detect_collision(treasure):
                 is_game_over = True
+                did_win = True
+                text = font.render("You win", True, black_color)
+                self.game_screen.blit(text, (275,350))
+                pygame.display.update()
+                clock.tick(1)
+                break
             
             # Update the game graphics
             pygame.display.update()
             clock.tick(self.tick_rate)
+        if did_win:
+            self.run_game_loop(level_speed + 0.5)
+        else:
+            return
 
 
 # Generic game object class to be subclassed by other objects in the game
@@ -108,8 +141,6 @@ class PlayerCharacter(GameObject):
         
         return True
 
-
-
 # Enemy character class
 class EnemyCharacter(GameObject):
     speed = 10
@@ -123,18 +154,12 @@ class EnemyCharacter(GameObject):
         elif self.xpos >= max_width - 70:
             self.speed = -abs(self.speed)
         self.xpos += self.speed
-        
-
 
 
 pygame.init()
 
-new_game = Game(screen_title, screen_width, screen_height)
-new_game.run_game_loop()
-
-
-
-
+new_game = Game("images/background.png", screen_title, screen_width, screen_height)
+new_game.run_game_loop(1)
 
 # Quite pygame and the program
 pygame.quit()
